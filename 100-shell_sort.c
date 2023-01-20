646d1a612c9ada4a59b53555c75a90a0c9f8b70c
Adedeji88 <mustaphadedeji@yahoo.com>
2023-01-20 06:24:27 -0800

    Hpt fix

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,34 +1,34 @@
 #include "sort.h"
-
-void insertion_sort(int *, long int, long int, size_t);
-
 /**
-* shell_sort - shell sort algorithm
-*
-* @array: array to sort
-* @size: size of array
-*/
-void shell_sort(int *array, size_t size)
+ * bubble_sort - sorts bubbly
+ * @array: array to sort
+ * @size: size
+ *
+ */
+void bubble_sort(int *array, size_t size)
 {
-	long int n = 1;
-	long int i, k, tmp;
+	size_t i, newsize = size;
+	int tmp, swap;
 
-	if (size < 2)
+	if (array == NULL || size < 2)
 		return;
 
-	while (n < (long int)size / 3)
-		n = n * 3 + 1;
-
-	while (n > 0)
+	while (newsize)
 	{
-		for (i = n; i < (long int) size; i++)
+		swap = 0;
+		for (i = 0; i < newsize - 1; i++)
 		{
-			tmp = array[i];
-			for (k = i; k >= n && array[k - n] > tmp; k = k - n)
-				array[k] = array[k - n];
-			array[k] = tmp;
+			if (array[i] > array[i + 1])
+			{
+				tmp = array[i + 1];
+				array[i + 1] = array[i];
+				array[i] = tmp;
+				print_array(array, size);
+				swap = 1;
+			}
 		}
-		print_array(array, size);
-		n = (n - 1) / 3;
+		newsize--;
+		if (!swap)
+			break;
 	}
 }
